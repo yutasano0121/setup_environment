91697611185e83e4f241281d4cc66c4e043b3c14
Yuta Asano <65088414+yutasano0121@users.noreply.github.com>
2021-04-15 15:06:49 -0700

    Create yum.sh

---
+++ yum.sh
@@ -0,0 +1,11 @@
+sudo apt-get install \
+    htop \
+    tmux \
+    openssl-devel \
+    libcurl-devel \
+    libxml2-devel
+    
+sudo amazon-linux-extras install R
+
+sudo yum update
+sudo yum upgrade
